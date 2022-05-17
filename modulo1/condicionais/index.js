8e0af53d5da9d6ef57e2bf18c2705c437a20711a
ArturTeixeira <arturroots92@gmail.com>
2022-05-17 15:20:04 -0300

    adicionado-condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,85 @@
+// Exercícios de interpretação de código
+
+//1-
+//a-Se o numero e dividido por 2 
+//b-numeros pares 
+//c-numeros impares
+
+
+//2-
+//a- dizer a fruta e o preço 
+//b- O preço da fruta, "FRUTA" é , "VALOR", preço
+//c- O preço da fruta  Pêra  é  R$  5.5
+
+
+//3
+//a-Pedindo para o usuario digitar um numero 
+//b-10 : Esse numero passou no teste // -10 erro
+
+
+
+
+//Exercícios de escrita de código
+
+//1
+//a-b-c
+// let idadeUsuario = Number(prompt('Qual sua idade ?'))
+
+// if (idadeUsuario >= 18){
+// console.log('Você pode dirigir')
+// }
+// else{
+    
+//     console.log('Você nao pode dirigir')
+// }
+
+
+
+//2-
+// let turno = prompt('Digitar M (matutino) ou V (Vespertino) ou N (Noturno)')
+
+// if (turno === 'M'){
+//     console.log('Bom dia!')
+
+// }
+// else if (turno === 'V'){
+
+//     console.log('Boa tarde!')
+// }
+// if (turno === 'N'){
+//     console.log('Boa noite!')
+// }
+
+
+
+//3-
+// let turno = prompt('Digitar M (matutino) ou V (Vespertino) ou N (Noturno)')
+
+// switch (turno) {
+//   case 'M':
+// console.log('Bom dia!') 
+//   break
+//   case 'V':
+// console.log('Boa Tarde!')
+//   break;
+//   case 'N':
+// console.log('Boa noite!')
+//     break;
+// }
+
+
+
+//4-
+// let generoFilme = prompt('Qual genero do filme?')
+// let precoFilme = Number(prompt('Qual preço do ingresso?'))
+
+
+// let condicaoFilme = (genero, valor) =>{
+//     if (genero === 'fantasia', valor < 15){
+//     console.log('Bom Filme!')
+// }
+// else {
+//     console.log('Escolha outro filme')
+// }
+// }   
+// condicaoFilme(generoFilme, precoFilme)
