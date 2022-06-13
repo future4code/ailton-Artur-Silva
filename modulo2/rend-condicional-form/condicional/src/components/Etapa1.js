cc246c7cf9d8d12951c7f084d5b5d6dbf992cf97
ArturTeixeira <arturroots92@gmail.com>
2022-06-13 16:56:18 -0300

    adicionado-condicional

---
+++ modulo2/rend-condicional-form/condicional/src/components/Etapa1.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Etapa1 extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Dados Gerais</h1> 
+      <p>1. Qual seu nome ?</p>
+      <input></input>
+     
+      <p>2. Qual sua idade ?</p>
+      <input></input>
+   
+      <p>3. Qual seu email ?</p>
+      <input></input>
+
+      <p>3. Qual sua escolaridade ?</p>
+      <input></input>
+
+      </div>
+      
+    )
+  }
+}
