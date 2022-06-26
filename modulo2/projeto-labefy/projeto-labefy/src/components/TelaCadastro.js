37d26a8e7ab84635c942ffbd89b81c36d05f4be6
ArturTeixeira <arturroots92@gmail.com>
2022-06-25 22:29:38 -0300

    adicionado-alguns-components

--- modulo2/projeto-labefy/projeto-labefy/src/components/TelaCadastro.js
+++ modulo2/projeto-labefy/projeto-labefy/src/components/TelaCadastro.js
@@ -1,23 +1,48 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 
 export default class TelaCadastro extends Component {
   state = {
     nome:''
-  }
+   }
   
   eventoNome = (event) => {
-    this.setState({nome: event.targert.value})
+    this.setState({nome: event.target.value})
   }
   
+  fazerPlaylist = () => {
+    const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists'
+    const body = {
+      name: this.state.nome
+    }
+    axios.post(url, body, {
+      headers: {
+      Authorization: 'artur-silva-ailton'
+      }
+    })
+    .then((res)=>{
+      alert('Playlist adicionada com sucesso!')
+      this.setState({nome:''})
+    })
+    .catch((err)=>{
+      alert(err.response.data.message)
+    })
+  }
+  
+ 
   
   render() {
     return (
       <div>
-        <button onClick={this.props.irParaLista}> Ver Playlist</button>
+        <button onClick={this.props.irParaLista}>Ver Playlist</button>
         <h1>Adicionar playlist</h1>
-        <input placeholder='Nome'/>
-        <button>Cadastrar</button>
+        <input 
+        placeholder='Nome da Playlist'
+        value={this.state.nome}
+        onChange={this.eventoNome}
+        />
+        <button onClick={this.fazerPlaylist}>Cadastrar</button>
       </div>
     )
   }
-}
+} 
