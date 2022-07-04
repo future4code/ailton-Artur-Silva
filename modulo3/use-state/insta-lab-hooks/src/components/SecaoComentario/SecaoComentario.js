3040f8f68b5cf76473bbe89c64e04bfb2dbedee5
ArturTeixeira <arturroots92@gmail.com>
2022-07-04 17:17:00 -0300

    adicionado-use-state

---
+++ modulo3/use-state/insta-lab-hooks/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import styled from "styled-components"
+
+
+const CommentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+const InputComment = styled.input `
+    width: 100%;
+    margin-right: 5px;
+`
+
+
+const SecaoComentario = (props) => {
+const [inputValue, setinputValue] = useState('')
+
+	const onChangeComentario = (event) => {
+      setinputValue (event.target.value)
+	console.log(event.target.value)
+	}
+
+	return (
+		<CommentContainer>
+			<InputComment
+				className={'comentario'}
+				placeholder={'Comentário'}
+				value={inputValue}
+				onChange={onChangeComentario}
+			/>
+			<button onClick={() => { props.enviarComentario() }} >Enviar</button>
+		</CommentContainer>
+	)
+}
+
+
+export default SecaoComentario
